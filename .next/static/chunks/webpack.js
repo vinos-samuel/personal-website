58c12502c0e3f5a21d3cae981368d938ebeb5a41
Vinos Smuel <89173053+vinos-samuel@users.noreply.github.com>
2025-06-05 14:21:08 +0800

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee955b22892bbfc2"; }
+/******/ 		__webpack_require__.h = function() { return "15d8b6f1fb6dd609"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
